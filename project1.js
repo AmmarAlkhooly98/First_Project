69108f09b1f802de3c55f51d6ddeae42fbd5a33e
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-03-27 17:07:40 +0200

    added OOP function

--- project1.js
+++ project1.js
@@ -2,15 +2,11 @@
 var participants = ["Abdulrohman Al-Akrad",'Ahmad Aldarkashli','Ammar Alkhooly','Ayman Alhariri','Besslan','Esraa Ramahi','Hasan Al-Abrash','Mohammad Elokour','Mohammad Jozaa','Yazan Eliwah','Yazan Alnajjar','Yousef (Joe)','Ahmad(coach)','Amer Alkharashgah','Areej Obaid','Basma Al-Tayeh','Canaan','Fatema','Hamza Olimat','Hamzeh Obiedat','Huda Al-Raee','Iman Elokour','Jomana Obaida','Laith Obeidat','Mahmoud','Medina Sheikh','Mohammad Hassan','Moatasem Fayez Alraggad','Oday Abueid','Qusai Alzunaymat','Raed Shhabeddin','Rafat Ahmed','Rawan Akram','Rubah Addad','Sahar','Samia Albess','Sara Bashaireh','Shareef','Wael','Yasser Qasem','Yazan Eliwah','Yousef Alshabah','Kelsey','Mr. Hugh Bosely','bayan Alraqqad','zainab Hammami','Seif Miehair'];                                 
 
 function randomName(myArray){
-   var r = [Math.floor(Math.random() * myArray.length)
-     myArray[r]
-
+   return  myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 $(document).ready(function(){
-
-    
-
+   
     $('#generaten').on('click',function(myArray){
 
       $('#whois').html ('')
